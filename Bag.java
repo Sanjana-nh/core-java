5bd3dad6c41e358fd9feff1776fba20509322037
Sanjana-2906 <dodawadsanjana29@gmail.com>
2026-02-05 13:42:08 +0530

    basics of java

---
+++ Bag.java
@@ -0,0 +1,26 @@
+public class Bag {
+    public static void main (String [] args)
+{
+System.out.println("Used in daily life");
+System.out.println("Found in verities");
+System.out.println("Used to hold anythings");
+System.out.println("Water Bottle,fruits,books,ect");
+System.out.println("Make of cotten , leather, etc");
+System.out.println("Bags include plastic cover ,sling bags , lugguage ,etc");
+System.out.println("All in one bags are popular ");
+System.out.println("Prices can range from 10rs to millions");
+System.out.println("I own 2 bags ");
+System.out.println("One is Laptop bag");
+System.out.println("Another is Sling bag ");
+System.out.println("My roommate owns 2 bags");
+System.out.println("One is blue ");
+System.out.println("one is black");
+System.out.println("Usually plastic bags are banned");
+System.out.println("Cotten bags are eco friendly");
+System.out.println("Jute bags are  good for carrying around");
+System.out.println(" bottles should be cleaned on regular basis");
+System.out.println("Carrying water bottels is important for staying hydrated inside bag");
+System.out.println("If you're a sportsmen its important carry big bottles ");
+}
+    
+}
---
+++ Cat.java
@@ -0,0 +1,26 @@
+public class Cat {
+        public static void main (String [] args)
+{
+System.out.println("Used in daily life");
+System.out.println("Found in verity of shapes");
+System.out.println("Used to fill liquids");
+System.out.println("Water,fruit juice,milk,ect");
+System.out.println("Make of plastic , steel or silicon , etc");
+System.out.println("Can fill hot water too");
+System.out.println("Copper water bottles are proven good to carry drinking water");
+System.out.println("Prices can range from 30rs to 1L");
+System.out.println("I own 2 water bottles ");
+System.out.println("One is red in colour");
+System.out.println("Another is black ");
+System.out.println("My roommate owns 2 bottles");
+System.out.println("One is blue ");
+System.out.println("one is black");
+System.out.println("Usually plastic water bottles cost 20rs per liter");
+System.out.println("Glass bottels are eco friendly");
+System.out.println("Glass bottles are fragile and not good for carrying around");
+System.out.println(" bottles should be cleaned on regular basis");
+System.out.println("Carrying water bottels is important for staying hydrated");
+System.out.println("If you're a sportsmen its important carry big bottles ");
+}
+    
+}
